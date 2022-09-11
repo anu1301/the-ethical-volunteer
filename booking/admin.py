aee9a1bf34dad9c1d22da2db5c2c4e478015b4fb
Anu Sethi <anu1301@hotmail.co.uk>
2022-09-11 11:47:49 +0000

    Create category and product model, migrate and create superuser

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'slug', 'price', 'available', 'created_on', 'updated')
+    search_fields = ['name']
+    list_filter = ['available', 'created_on', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+  
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    content = models.TextField()
+    available = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
