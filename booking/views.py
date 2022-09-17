116b6fb43da6ff2fcfc1ec87248a0afa0e203a9a
Anu Sethi <anu1301@hotmail.co.uk>
2022-09-17 11:18:48 +0000

    Create login in view.py, create forms.py, add urls path in main urls.py and app urls.py

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+# Generic views imported from django
 from django.views import generic, View
+# Product and Booking models imported from models.py
 from .models import Product, Booking
-
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 
 class HomePage(View):
@@ -24,3 +27,21 @@ class HolidayView(View):
             request,
             "holiday.html", 
             {"holiday_active": "user-redirect"})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Invalid login')
+            else:
+                form = LoginForm()
+            return render(request, 'booking/login.html', {'form': form})
